765cc9cc500b5d0c4b8faf31bfec5ee011a86fb8
Sri Varshith <nvarshith.2007@gmail.com>
2025-06-13 23:20:58 +0530

    card displaying temperature and weather added

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 
@@ -13,7 +15,7 @@ class weatherscreen extends StatelessWidget{
           style: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 27,
-            color: color_for_items_in_appbar
+            color: Colors.white
           ),
         ),
         actions: [
@@ -24,17 +26,64 @@ class weatherscreen extends StatelessWidget{
             icon: Icon(
               Icons.refresh,
               size: 32,
-              color: color_for_items_in_appbar,
-              
-
-            )
-            
+              color: Colors.white,
+            )           
             )
           ],
         centerTitle: true,
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          //main display
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: ClipRRect(
+                borderRadius: BorderRadiusGeometry.circular(12),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 10,sigmaY: 10),
+                  child: Card(
+                    elevation: 2,
+                      color : const Color.fromARGB(255, 56, 62, 74),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(19)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text("310 K",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30
+                            ),
+                          ),
+                          Icon(
+                            color: Colors.white,
+                            Icons.cloud,
+                            size: 80,
+                          ),
+                          Text("Cloudy",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold
+                            ),
+                      
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
 
-
+          ],
+        ),
+      ),
     );
   }
 
